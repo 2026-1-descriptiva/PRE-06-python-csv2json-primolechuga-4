0a3105f4d1a0020d25d9cbc3b889cf54e1bf9a07
Juan Esteban Cadavid Arango <jucadavida@unal.edu.co>
2026-04-10 17:41:05 -0500

    initial commit

---
+++ homework/__init__.py
@@ -0,0 +1,16 @@
+import csv
+import json
+
+def convert_csv_to_json():
+    csv_file = 'files/drivers.csv'
+    json_file = 'files/drivers.json'
+    data = []
+    with open(csv_file, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data.append(row)
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    convert_csv_to_json()
---
+++ homework/__main__.py
@@ -0,0 +1,3 @@
+from . import convert_csv_to_json
+
+convert_csv_to_json()
